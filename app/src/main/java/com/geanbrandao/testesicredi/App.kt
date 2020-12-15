83fafc9256ce93b029da820f385fefe15369ecce
gean.kar <gean.kar@brave.ag>
2020-12-15 13:28:20 -0300

    app center added

--- app/src/main/java/com/geanbrandao/testesicredi/App.kt
+++ app/src/main/java/com/geanbrandao/testesicredi/App.kt
@@ -4,14 +4,23 @@ import androidx.multidex.MultiDexApplication
 import com.geanbrandao.testesicredi.module.adapterModule
 import com.geanbrandao.testesicredi.module.repositoryModule
 import com.geanbrandao.testesicredi.module.viewModelModule
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import timber.log.Timber
 
+
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+
+        AppCenter.start(
+            this, "2678871c-8310-42cd-b392-fb5c357026b4",
+            Analytics::class.java, Crashes::class.java
+        )
 
         startKoin {
             androidLogger()
